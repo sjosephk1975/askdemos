f48623836aa8233ddbf42800987b3dccb65619b7
sjosephk1975@gmail.com <sjosephk1975@gmail.com>
2022-05-01 10:24:17 +0100

    ruboautocorrect

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,8 +1,7 @@
 class Inbox < ApplicationRecord
-    MIN_NAME = 6
-    MAX_NAME = 100
-    validates :name, presence: true
-    validates :name, uniqueness: true
-    validates :name, length: { in: MIN_NAME..MAX_NAME }
-
+  MIN_NAME = 6
+  MAX_NAME = 100
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :name, length: { in: MIN_NAME..MAX_NAME }
 end
