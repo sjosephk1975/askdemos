ccda8017ba4d7a979990a774eb197d8ba8455160
sjosephk1975@gmail.com <sjosephk1975@gmail.com>
2022-05-02 12:16:06 +0100

    Added basic css styling

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,6 +1,6 @@
 class Inbox < ApplicationRecord
   MIN_NAME = 6
-  MAX_NAME = 100
+  MAX_NAME = 1000
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { in: MIN_NAME..MAX_NAME }
