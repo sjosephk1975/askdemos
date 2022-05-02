db435eac4724099f3bb4b4e726ad3ff058df6279
sjosephk1975@gmail.com <sjosephk1975@gmail.com>
2022-05-02 21:24:25 +0100

    user inboxes dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :inboxes
+  has_many :inboxes, dependent: :destroy
 end
