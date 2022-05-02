e76cdf5646411beca273ea06c554c1df973ae083
sjosephk1975@gmail.com <sjosephk1975@gmail.com>
2022-05-02 16:30:39 +0100

    associated inboxes and users

--- app/models/inbox.rb
+++ app/models/inbox.rb
@@ -1,6 +1,9 @@
 class Inbox < ApplicationRecord
+  belongs_to :user
+
   MIN_NAME = 6
   MAX_NAME = 1000
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: { in: MIN_NAME..MAX_NAME }
