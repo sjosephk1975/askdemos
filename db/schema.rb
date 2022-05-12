9c86db3320bf34f84c0e2308041db0b68e38c619
sjosephk1975@gmail.com <sjosephk1975@gmail.com>
2022-05-12 06:41:29 +0100

    counter cache messages to inbox

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_02_203017) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_12_050811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_02_203017) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.integer "messages_count", default: 0, null: false
     t.index ["name"], name: "index_inboxes_on_name", unique: true
     t.index ["user_id"], name: "index_inboxes_on_user_id"
   end
