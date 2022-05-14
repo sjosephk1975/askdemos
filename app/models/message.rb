1a529c4031ea793b16ed38cb13dee92d590087b5
sjosephk1975@gmail.com <sjosephk1975@gmail.com>
2022-05-14 21:04:30 +0100

    gem acts as votable, vote for messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,4 +8,5 @@ class Message < ApplicationRecord
   validates :body, presence: true
   # validates :body, uniqueness: { scope: :inbox_id }
   validates :body, length: { in: MIN_BODY..MAX_BODY }
+  acts_as_votable
 end
