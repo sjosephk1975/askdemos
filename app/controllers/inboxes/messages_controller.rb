d141b3b1df766490fed90fd84587be115679bafb
sjosephk1975@gmail.com <sjosephk1975@gmail.com>
2022-05-22 12:51:44 +0100

    update message count at inbox on destroy

--- app/controllers/inboxes/messages_controller.rb
+++ app/controllers/inboxes/messages_controller.rb
@@ -49,9 +49,13 @@ module Inboxes
       @message = @inbox.messages.find(params[:id])
       @message.destroy
       respond_to do |format|
-        format.turbo_stream
+        format.turbo_stream do
+            render turbo_stream: [
+             turbo_stream.remove(@message),
+             turbo_stream.update('message_counter', @inbox.messages_count),
+            ]
+        end
         format.html { redirect_to @inbox, notice: 'Message was successfully destroyed.' }
-        format.json { head :no_content }
       end
     end
 
